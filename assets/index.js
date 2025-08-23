df01a04ce8e62b88ff3d1872d7e9869b0884db55
fajarajikusuma <fajikusuma@gmail.com>
2025-08-23 22:03:42 +0700

    alert while off js

--- assets/index.js
+++ assets/index.js
@@ -32,4 +32,19 @@ document.querySelectorAll('a').forEach(anchor => {
     });
 });
 
+// cegah agar tidak bisa download gambar, copy, drag and drop, inspect element, view source code, dan mematikan javascript web tidak bisa
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', event => {
+    if (event.key === 'F12' ||
+        (event.ctrlKey && event.shiftKey && ['I', 'J', 'C'].includes(event.key)) ||
+        (event.ctrlKey && event.key === 'U')) {
+        event.preventDefault();
+    }
+});
+document.addEventListener('dragstart', event => event.preventDefault());
+document.addEventListener('copy', event => event.preventDefault());
+document.addEventListener('cut', event => event.preventDefault());
+document.addEventListener('paste', event => event.preventDefault());
+document.addEventListener('selectstart', event => event.preventDefault());
+
 
